a380ba24e4688fd19a3d3f2bb8c7dcb5f9f685cd
Sirsha2468 <69218962+Sirsha2468@users.noreply.github.com>
2020-11-05 02:03:26 +0530

    Add files via upload

--- Programe/Structure of students marks.cpp
+++ Programe/Structure of students marks.cpp
@@ -1,1 +1,52 @@
+#include<stdio.h>
 
+struct student{
+	
+	char name[10];
+	int roll;
+	float marks;	
+
+}sum[10];
+
+int main()
+
+{
+	
+int i;
+printf("Enter the details of the students:");
+
+
+for(i=0 ; i< 4 ; i++)
+
+{
+	sum[i].roll = i + 1;
+	printf("\n For roll number is: %d\n",sum[i].roll);
+
+	printf("\nEnter the name of the student: ");
+	scanf("%s",&sum[i].name);
+	
+	printf("\nEnter the marks: ");
+	scanf("%f",&sum[i].marks);
+	}
+	
+	printf("\n\t\t\t\t Your enterd details is:\n");
+	
+	for(i=0 ; i< 4 ; ++i)
+
+	{
+		printf("\nFor roll number: %d",i+1);
+		
+		printf("\nName of the student: ");
+		puts(sum[i].name);
+		
+	printf("\nMarks is:%f ",sum[i].marks);
+	printf("\n");
+		
+	}
+	return 0;
+}
+	
+	
+	
+	
+	
